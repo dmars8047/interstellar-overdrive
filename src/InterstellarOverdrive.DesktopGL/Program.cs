a93d7012c77b0233edb6cc02ee15b6c62454d090
dmars8047 <dmars8047@live.com>
2022-04-12 22:37:00 -0400

    First Commit! Just the how to program.

---
+++ src/InterstellarOverdrive.DesktopGL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InterstellarOverdrive.DesktopGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new InterstellarOverdriveGame())
+                game.Run();
+        }
+    }
+}
